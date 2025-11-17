3ba2ab7d9ef0b077b8abfa11428d72865b4d51c5
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-17 13:14:42 -0800

    Update preview.tsx

--- apps/docs/components/custom/preview.tsx
+++ apps/docs/components/custom/preview.tsx
@@ -85,7 +85,7 @@ export const Preview = async ({ path, className }: ComponentPreviewProps) => {
         <CodeBlockTabsTrigger value="code">Code</CodeBlockTabsTrigger>
       </CodeBlockTabsList>
       <CodeBlockTab className="not-prose p-0" value="preview">
-        <ResizablePanelGroup direction="horizontal">
+        <ResizablePanelGroup direction="horizontal" id={`preview-${path}`}>
           <ResizablePanel defaultSize={100}>
             <div className={cn("h-[600px] overflow-auto p-4", className)}>
               <Component />
